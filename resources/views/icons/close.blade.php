521c3723dbb37d96c1829c253c172e8b15ec5038
Aleks <my@mail.com>
2023-11-29 04:39:58 +0300

    add icons

---
+++ resources/views/icons/close.blade.php
@@ -0,0 +1,4 @@
+<svg {{ $attributes }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+    <path d="M22.5 7.5L7.5 22.5" stroke="#C4C4C4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M7.5 7.5L22.5 22.5" stroke="#C4C4C4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+</svg>
